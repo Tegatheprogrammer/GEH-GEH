4dc12b0e5ec0e69667825cecaeaaab1c884d8d94
Tegatheprogrammer <139498675+Tegatheprogrammer@users.noreply.github.com>
2025-01-15 14:02:32 +0100

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,150 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+let allMusic = [
+  {
+    name: "2billion",
+    artist: "GEH GEH",
+    img: "geh-1",
+    src: "geh-1",
+  },
+  {
+    name: "  advice",
+    artist: " GEH GEH",
+    img: "geh-2",
+    src: "geh-2",
+  },
+  {
+    name: "avoid me ",
+    artist: "GEH GEH",
+    img: "geh-3",
+    src: "geh-3",
+  },
+  {
+    name: "Belle",
+    artist: "GEH GEH",
+    img: "geh-4",
+    src: "geh-4",
+  },
+  {
+    name: "Children",
+    artist: "GEH GEH ",
+    img: "geh-5",
+    src: "geh-5",
+  },
+  {
+    name: "Eat",
+    artist: "GEH GEH",
+    img: "geh-6",
+    src: "geh-6",
+  },
+
+  
+    {
+    name: "work",
+    artist: "GEH GEH",
+    img: "geh-7",
+    src: "geh-7",
+  },
+  
+    {
+    name: "wisdom",
+    artist: "GEH GEH",
+    img: "geh-8",
+    src: "geh-8",
+  },
+    {
+    name: "wicked man",
+    artist: "GEH GEH",
+    img: "geh-9",
+    src: "geh-9",
+  },
+    {
+    name: "Weight",
+    artist: "GEH GEH",
+    img: "geh-10",
+    src: "geh-10",
+  },
+    {
+    name: "Twerk",
+    artist: "GEH GEH",
+    img: "geh-11",
+    src: "geh-11",
+  },
+    {
+    name: "turkey",
+    artist: "GEH GEH",
+    img: "geh-12",
+    src: "geh-12",
+  },
+    {
+    name: "Send You For Here",
+    artist: "GEH GEH",
+    img: "geh-13",
+    src: "geh-13",
+  },
+    {
+    name: "Pour spit",
+    artist: "GEH GEH",
+    img: "geh-14",
+    src: "geh-14",
+  },
+    {
+    name: "picture",
+    artist: "GEH GEH",
+    img: "geh-15",
+    src: "geh-15",
+  },
+    {
+    name: "Pick you",
+    artist: "GEH GEH",
+    img: "geh-16",
+    src: "geh-16",
+  },
+    {
+    name: "opueh",
+    artist: "GEH GEH",
+    img: "geh-17",
+    src: "geh-17",
+  }, {
+    name: "offence",
+    artist: "GEH GEH",
+    img: "geh-18",
+    src: "geh-18",
+  },
+    {
+    name: "money",
+    artist: "GEH GEH",
+    img: "geh-19",
+    src: "geh-19",
+  },  {
+    name: "lets be guided",
+    artist: "GEH GEH",
+    img: "geh-20",
+    src: "geh-20",
+  },
+    {
+    name: "leave here",
+    artist: "GEH GEH",
+    img: "geh-21",
+    src: "geh-21",
+  },
+  {
+    name: "hand",
+    artist: "GEH GEH",
+    img: "geh-22",
+    src: "geh-22",
+  }, {
+    name: "Empower",
+    artist: "GEH GEH",
+    img: "geh-23",
+    src: "geh-23",
+  },
+    
+];
